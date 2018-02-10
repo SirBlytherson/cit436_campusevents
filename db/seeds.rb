949c210cf2fc5a67f1ca83b63217e8b0a9309642
blrbrown <blrbrown@iupui.edu>
2018-02-10 00:16:29 +0000

    Added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,30 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#i   Character.create(name: 'Luke', movie: movies.first)
+
+# 2 Events
+e1 = Event.create(name: 'CarnivalBizzarro', start_date: , end_date: 1518220793);
+e2 = Event.create(name: 'FestivalSpectacular', start_date: , end_date: 1518220793);
+
+# 4 locations
+l1 = Location.create(name: 'Indiana');
+l2 = Location.create(name: 'Oregon');
+l3 = Location.create(name: 'Utah');
+l4 = Location.create(name: 'Wyoming');
+
+# 4 meetings
+m1 = Meeting.create(name: 'HoosierFest', start_time: , end_time: 1518220793, location_id: l1);
+m2 = Meeting.create(name: 'OregonTrail', start_time: , end_time: 1518220793, location_id: l2);
+m3 = Meeting.create(name: 'DesertClimb', start_time: , end_time: 1518220793, location_id: l3);
+m4 = Meeting.create(name: 'WyMiRoaming', start_time: , end_time: 1518220793, location_id: l4);
+
+# 2 sponsors
+s1 = Sponsor.create(name: 'Rich Uncle');
+s2 = Sponsor.create(name: 'Brandon');
+
+# 4 Attendee
+a1 = Attendee.create(first_name: 'John', last_name: 'Doe', email: 'jn.doe@gmail.com');
+a2 = Attendee.create(first_name: 'Jane', last_name: 'Doe', email: 'ja.doe@gmail.com');
+a3 = Attendee.create(first_name: 'Rob', last_name: 'Elliot', email: 'elliot@iupui.edu');
+a4 = Attendee.create(first_name: 'Blythe', last_name: 'Brown', email: 'whatsittoya@iupui.edu');
