76509deb802dbea83c9a2545cc9fd3af6132ba7c
blrbrown <blythe.brown@sbcglobal.net>
2018-02-15 10:21:17 -0500

    Clearing validation issue

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,12 @@
 class Meeting < ApplicationRecord
+    validates :name, :start_time, :end_time, :presence => true
+    validate :check_datetime_order
+    belongs_to :location
+    belongs_to :event
+
+  def check_datetime_order
+    if start_date? && end_date? && end_date <= start_date
+      errors.add(:unordered_dates, ": Start Date must be before End Date")
+    end
+  end
 end
