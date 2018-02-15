ca69d2831ca5c503d42c9af2aa560046c7050453
blrbrown <blythe.brown@sbcglobal.net>
2018-02-15 13:58:44 -0500

    Attempt to upadte location show view

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,16 @@
 class Location < ApplicationRecord
     validates :name, :presence => true
     has_many :meetings
+	
+  def in_order
+    order(start_time: :asc)
+  end
+
+  def recent(n)
+    in_order.endmost(n)
+  end
+
+  def endmost(n)
+    all.only(:order).from(all.reverse_order.limit(n), location)
+  end
 end
