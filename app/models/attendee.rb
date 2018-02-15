d7a92fbaeedde6925c851fe4ee53e2c671506ce9
blrbrown <blythe.brown@sbcglobal.net>
2018-02-14 22:39:11 -0500

    Hopefully resolve email validatio issue

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,5 +1,5 @@
 class Attendee < ApplicationRecord
-    validates :first_name, :last_name, :email, :required => true
+    validates :first_name, :last_name, :email
     has_many :subscriptions
     has_many :events, :through => :subscriptions
 end
