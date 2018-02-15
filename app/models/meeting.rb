47d69d4724762408c7ae95ae3d39898894823e74
blrbrown <blythe.brown@sbcglobal.net>
2018-02-15 14:04:18 -0500

    Attempt to upadte location show view

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -9,16 +9,4 @@ class Meeting < ApplicationRecord
       errors.add(:unordered_dates, ": Start Date must be before End Date")
     end
   end
-	
-  def in_order
-    order(start_time: :asc)
-  end
-
-  def recent(n)
-    in_order.endmost(n)
-  end
-
-  def endmost(n)
-    all.only(:order).from(all.reverse_order.limit(n), :location)
-  end
 end
