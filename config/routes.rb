3d776feb4105930e30871977f9cc5d9d8ceb64b3
SirBlytherson <blythe.brown@sbcglobal.net>
2018-02-27 14:41:41 -0500

    Build/config home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   resources :meetings
   resources :sponsors
   resources :events
-  
-  root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
